4c640f7680efabb26b02dbc451b51bf2d08fce13
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-11-22 09:47:19 -0300

    Update vala.c

--- vala.c
+++ vala.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define max_lista 100
+#define max_nome 21
+
+typedef struct{
+    char nome[max_nome];
+}Kid;
 
 int main(void){
-  printf("Hello World");
-  
-  return 0;
-  
- }
+    int number,comportou,nao_comportou;
+    char especial;
+    Kid lista[max_lista];
+    scanf("%d",&number);
+    for(int i = 0,comportou = 0, nao_comportou = 0;i < number;i++){
+        scanf("%c %s",&especial,lista[i].nome);
+        if(especial == '+'){
+            comportou++;
+        }else{
+            nao_comportou++;
+        }
+        for(int j = 0; j < number; i++){
+            for(int k = j + 1; k < number; k++){
+                if(strcmp(vetor[k], vetor[j]) == -1){
+                    strcpy(aux, vetor[j]);
+                    strcpy(vetor[i], vetor[k]);
+                    strcpy(vetor[k], max_nome);
+            }
+            }
+        }
+            for (int i = 0; i < number;i++)
+                printf("%s\n",lista[i].nome);
+            printf("se comportaram: %d | Nao se comportaram: %d\n",comportou,nao_comportou);
+        return 0;
+}
