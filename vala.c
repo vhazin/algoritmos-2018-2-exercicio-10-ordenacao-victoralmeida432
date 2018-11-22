2f0cad126255bb0d11686f917d496baee42e8bba
Victor Almeida <38266337+victoralmeida432@users.noreply.github.com>
2018-11-22 10:22:01 -0300

    Update vala.c

--- vala.c
+++ vala.c
@@ -1,38 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define max_lista 100
-#define max_nome 21
 
-typedef struct{
-    char nome[max_nome];
-}Kid;
+#define nome_max 21
+#define max_kids 100
+
+typedef struct kids{
+    char nome[nome_max];
+}kid;
+
+void swap (kid *head, kid *tail){
+    kid temp = *head;
+    *head = *tail;
+    *tail = temp;
+}
+
+int partition (kid array[],int menor, int maior){
+    int i = (menor - 1), x = 0;
+    for (int j = menor; j <= maior - 1;j++){
+        if (strcmp(array[j].nome,array[maior].nome) <= 0){
+            i++;
+            swap(&array[i],&array[j]);
+        }
+    }
+    swap(&array[i + 1],&array[maior]);
+    return (i + 1);
+}
+
+void quickSort(kid array[],int menor,int maior){
+    if (menor < maior){
+        int pi = partition(array,menor,maior);
+        quickSort(array,menor,pi - 1);
+        quickSort(array,pi + 1,maior);
+
+    }
+}
 
 int main(void){
-    int number,comportou,nao_comportou,aux,j,k;
-    char especial;
-    Kid lista[max_lista];
-    scanf("%d",&number);
-    for(int i = 0,comportou = 0, nao_comportou = 0;i < number;i++){
-        scanf("%c %s",&especial,lista[i].nome);
-        if(especial == '+'){
-            comportou++;
-        }else{
-            nao_comportou++;
-        }
-        for(j = 0; j < number; i++){
-            for(k = j + 1; k < number; k++){
-                if(strcmp(max_lista[k], max_lista[j]) == -1){
-                    strcpy(aux, max_lista[j]);
-                    strcpy(max_lista[i], max_lista[k]);
-                    strcpy(max_lista[k], max_nome);
-            }
-            }
-        }
-            for (int i = 0; i < number;i++)
-                printf("%s\n",lista[i].nome);
-            printf("se comportaram: %d | Nao se comportaram: %d\n",comportou,nao_comportou);
-        
-        
-        return 0;
+    int tamanho;
+    char especial,cont = 0;
+    kid kidslist[max_kids];
+    scanf("%d",&tamanho);
+    for(int i = 0; i < tamanho;i++){
+        scanf(" %c ",&especial);
+        if(especial == '+')
+            cont++;
+        scanf("%s",kidslist[i].nome); 
+    }
+    quickSort(kidslist,0,tamanho - 1);
+    for (int x = 0; x < tamanho; x++){
+        printf("%s\n",kidslist[x].nome);
+    }
+    printf("Se comportaram: %d | Nao se comportaram: %d\n",cont,tamanho - cont);
+
+    return 0;
 }
